1e37db7f3189804daea3269c62645c5b4944e7ba
Yahya <yahya.19of99@gmail.com>
2023-03-02 16:49:47 +0500

    fixing some authentication bugs

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class PostBase(BaseModel):
     title: str
@@ -34,3 +36,10 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
