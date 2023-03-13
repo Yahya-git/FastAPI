7f064fe5c3563781325ef536ddc5ecd5b5191f3a
Yahya <yahya.19of99@gmail.com>
2023-03-13 18:40:20 +0500

    added 'posted_at' column to comments table

---
+++ alembic/versions/f2a167ddef8e_add_posted_at_column_to_comments_table.py
@@ -0,0 +1,27 @@
+"""add posted_at column to comments table
+
+Revision ID: f2a167ddef8e
+Revises: b41f608682de
+Create Date: 2023-03-09 13:25:37.679675
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f2a167ddef8e'
+down_revision = 'b41f608682de'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('comments', sa.Column('posted_at', sa.TIMESTAMP(
+        timezone=True), nullable=False, server_default=sa.text('NOW()')))
+    pass
+
+
+def downgrade() -> None:
+    op.drop_column('comments', 'posted_at')
+    pass
