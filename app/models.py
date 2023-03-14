410ae4baf617c8ed1187bc2fb06185e6a227b9a7
Yahya <yahya.19of99@gmail.com>
2023-03-14 15:35:41 +0500

    removed comments and used trunk linter to fix formatting

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import relationship
-from .database import Base
 from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String, text
+from sqlalchemy.orm import relationship
+
+from .database import Base
 
 
 class Post(Base):
@@ -9,11 +10,13 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    publish = Column(Boolean, server_default='TRUE')
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('NOW()'))
-    user_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), nullable=False)
+    publish = Column(Boolean, server_default="TRUE")
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
+    )
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
 
     user = relationship("User")
     likes = relationship("Likes")
@@ -26,24 +29,20 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('NOW()'))
-    
-    # post = relationship("Post")
-    # likes = relationship("Likes")
-    # comments = relationship("Comments")
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
+    )
 
 
 class Likes(Base):
     __tablename__ = "likes"
 
-    user_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), primary_key=True)
-    post_id = Column(Integer, ForeignKey(
-        "posts.id", ondelete="CASCADE"), primary_key=True)
-    
-    # user = relationship("User")
-    # post = relationship("Post")
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
 
 
 class Comments(Base):
@@ -52,8 +51,6 @@ class Comments(Base):
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"))
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     comment = Column(String, nullable=False)
-    posted_at = Column(TIMESTAMP(timezone=True),
-                       nullable=False, server_default=text('NOW()'))
-    
-    # user = relationship("User")
-    # post = relationship("Post")
+    posted_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
+    )
